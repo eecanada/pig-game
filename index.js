fec2e218059e6bf889011c829cc7902f8a885fc0
Eder Canada <canada.eder@gmail.com>
2020-01-06 15:36:47 -0800

    add game state variable to stop game once game is won

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-let scores,currentScore, activePlayer, gamePlaying
-scores = [0,0]
-currentScore = 0
-activePlayer = 0
-gamePlaying = true 
+let scores, currentScore, activePlayer, gamePlaying
 
-document.querySelector('.dice').style.display = 'none'
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+init()
 
-document.querySelector('.btn-roll').addEventListener('click', function(){
-  const dice = Math.floor(Math.random() * 6) + 1
-  const diceDOM = document.querySelector('.dice')
-  diceDOM.style.display = 'block'
-  diceDOM.src = `dice-${dice}.png`
-  if(dice !== 1){
-    currentScore = currentScore + dice
-    document.getElementById(`current-${activePlayer}`).textContent = currentScore
-  } else {
-    nextPlayer()
+document.querySelector('.btn-roll').addEventListener('click',
+
+function(){
+  if(gamePlaying){
+    const dice = Math.floor(Math.random() * 6) + 1
+    const diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block'
+    diceDOM.src = `dice-${dice}.png`
+    if(dice !== 1){
+      currentScore = currentScore + dice
+      document.getElementById(`current-${activePlayer}`).textContent = currentScore
+    } else {
+      nextPlayer()
+    }
   }
-
 })
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
-  scores[activePlayer] = scores[activePlayer] + currentScore
-  document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
-  if(scores[activePlayer] >= 10 ){
-    document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
-    document.querySelector('.dice').style.display = 'none'
-    document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
-    document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
-  } else {
-    nextPlayer()
+  if (gamePlaying){
+    scores[activePlayer] = scores[activePlayer] + currentScore
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
+    if(scores[activePlayer] >= 10 ){
+      document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
+      document.querySelector('.dice').style.display = 'none'
+      document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
+      document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
+      gamePlaying = false
+    } else {
+      nextPlayer()
+    }
   }
 })
+
+function nextPlayer (){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  currentScore = 0
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+  document.querySelector('.player-0-panel').classList.toggle('active')
+  document.querySelector('.player-1-panel').classList.toggle('active')
+  document.querySelector('.dice').style.display = 'none'
+}
 
 document.querySelector('.btn-new').addEventListener('click',init)
 
@@ -45,7 +53,7 @@ function init (){
   currentScore = 0
   gamePlaying = true
   document.querySelector('.dice').style.display = 'none'
-  document.getElementById('.score-0').textContent = '0'
+  document.getElementById('score-0').textContent = '0'
   document.getElementById('score-1').textContent = '0'
   document.getElementById('current-0').textContent = '0'
   document.getElementById('current-1').textContent = '0'
@@ -59,12 +67,3 @@ function init (){
   document.querySelector('.player-0-panel').classList.add('active')
 }
 
-function nextPlayer (){
-  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-  currentScore = 0
-  document.getElementById('current-0').textContent = '0'
-  document.getElementById('current-1').textContent = '0'
-  document.querySelector('.player-0-panel').classList.toggle('active')
-  document.querySelector('.player-1-panel').classList.toggle('active')
-  document.querySelector('.dice').style.display = 'none'
-}
