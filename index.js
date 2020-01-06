4d15f1ebb0e63cb753c42ab61d6ba6d5b1e8a414
Eder Canada <canada.eder@gmail.com>
2020-01-06 15:07:24 -0800

    set up hold function and winner logic

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 })
 
+document.querySelector('.btn-hold').addEventListener('click',function(){
+  scores[activePlayer] = scores[activePlayer] + currentScore
+  document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
+  if(scores[activePlayer] >= 10 ){
+    document.querySelector(`#name-${activePlayer}`).textContent = 'Winner!'
+    document.querySelector('.dice').style.display = 'none'
+    document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
+    document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
+  } else {
+    nextPlayer()
+  }
+})
 
 function nextPlayer (){
   activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
