23dcaf8aff1c52e0b4af841bab31ec8a42ef648a
Eder Canada <canada.eder@gmail.com>
2020-01-06 14:45:55 -0800

    complete btn roll fuction and toggle player turns

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-let scores, roundScore, activePlayer
+let scores,currentScore, activePlayer
 scores = [0,0]
-roundScore = 0
+currentScore = 0
 activePlayer = 0
 
 document.querySelector('.dice').style.display = 'none'
-document.getElementById('score-0').textContent = 0
-document.getElementById('score-1').textContent = 0
-document.getElementById('current-0').textContent = 0
-document.getElementById('current-1').textContent = 0
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
 
+document.querySelector('.btn-roll').addEventListener('click', function(){
+  const dice = Math.floor(Math.random() * 6) + 1
+  const diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block'
+  diceDOM.src = `dice-${dice}.png`
+  if(dice !== 1){
+    currentScore = currentScore + dice
+    document.getElementById(`current-${activePlayer}`).textContent = currentScore
+  } else {
+    nextPlayer()
+  }
+
+})
+
+
+function nextPlayer (){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+  currentScore = 0
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+  document.querySelector('.player-0-panel').classList.toggle('active')
+  document.querySelector('.player-1-panel').classList.toggle('active')
+  document.querySelector('.dice').style.display = 'none'
+}
